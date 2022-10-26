3e910bfe6d264432e4b469067347173d63d8e5fb
parhamsoltani <70743729+parhamsoltani@users.noreply.github.com>
2022-10-26 09:55:36 +0330

    Update Q1.py

--- Session2/Q1.py
+++ Session2/Q1.py
@@ -4,15 +4,8 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-sides = []
-sides.append(a)
-sides.append(b)
-sides.append(c)
-
-sides.sort()
-
-if sides[0]**2 + sides[1]**2 == sides[2]**2:
+if (a ** 2 + b ** 2 == c ** 2) or (a ** 2 + c ** 2 == b ** 2) or (b ** 2 + c ** 2 == a ** 2):
     print("YES")
-
 else:
     print("NO")
+
